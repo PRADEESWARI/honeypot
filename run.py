6d1231d36ff8f1639ecee9ffd3d810b1e3bba77a
PRADEESWARI <rpradeeswari@gmail.com>
2024-12-18 12:11:26 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import create_app, db, socketio
 from app.models import FailedLogin
+import os
 
 app = create_app()
 
@@ -8,4 +9,6 @@ with app.app_context(): #database
     db.create_all()
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
     socketio.run(app, debug=True)
